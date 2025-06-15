be8015c9f53ec9da28414628ef489124ce9fc3a3
blezz-tech <markus.jenya04@yandex.ru>
2025-06-15 13:57:28 +0300

    Доработка пользователя

---
+++ laravel_1/database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // admin для удобства
+        User::factory()->create([
+            'login' => 'admin1',
+            'email' => 'admin1@admin.admin',
+            'password' =>  Hash::make('123'),
+            'is_admin' => 1,
+        ]);
+
+        // admin по заданию !!! Важно, чтобы всё совпадало с заданием
+        User::factory()->create([
+            'login' => 'help',
+            'email' => 'admin@admin.admin',
+            'password' =>  Hash::make('helpme'),
+            'is_admin' => 1,
+        ]);
+
+        // Создаём пользователя, которого будем тестировать
+        $user = User::factory()->create([
+            'login' => 'user',
+            'email' => 'user@user.user',
+            'password' =>  Hash::make('123'),
+        ]);
+
+        // Создаём ещё 10 случный пользователей
+        User::factory()->count(10)->create([
+            'password' => Hash::make('123'),
+        ]);
+    }
+}

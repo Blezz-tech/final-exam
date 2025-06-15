54396d8bdbb0f56f179159b0dfac3f9cc33a0c9f
blezz-tech <markus.jenya04@yandex.ru>
2025-06-15 14:23:47 +0300

    create prefab project

---
+++ prefab/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.home');
+})->name('home');
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [AuthController::class, 'createform'])->name('auth.createform');
+    Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
+
+    Route::get('/login', [AuthController::class, 'loginform'])->name('auth.loginform');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
+
+Route::middleware(['admin'])->group(function () {
+    // Запросы, которые может делать только админ
+    // И страницы админа
+});
